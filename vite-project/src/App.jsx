f23f89c8c42f9d911ed3e29293dc876839648886
Shivam Maheshwari <shivammaheshwari201@gmail.com>
2024-10-15 20:30:29 +0530

    Day1 commits

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+
+function App() {
+
+  return (
+    <>
+      <h1>Hy from Shivam</h1>
+      <Chai/>
+    </>
+  );
+}
+
+export default App
---
+++ vite-project/src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h1>Chai With Shivam</h1>
+    );
+}
+
+export default Chai;
